c1b881cc5030c9d1bf5fa552529fd562228bdb00
Jim Wardlaw <jim.wardlaw@gmail.com>
2017-03-06 21:16:10 +0000

    Feat (app): Added smd-app component
    This component will be used as the basis for all material applications

---
+++ addon/components/smd-app.js
@@ -0,0 +1,49 @@
+import Ember from 'ember';
+import layout from '../templates/components/smd-app';
+
+const {
+  Component,
+  computed,
+} = Ember;
+
+export default Component.extend({
+  // Attributes
+  layout,
+  classNames: [
+    'mdl-layout',
+    'mdl-js-layout',
+  ],
+  classNameBindings: [
+    'fixedDrawerClass',
+    'fixedHeaderClass',
+    'noDrawerButtonClass',
+    'noDesktopDrawerButtonClass',
+  ],
+  fixedDrawer: false, // Makes the drawer always visible and open in larger screens
+  fixedHeader: false, // Makes the header always visible, even in small screens
+  noDrawerButton: false, // Does not display a drawer button
+  noDesktopDrawerButton: false, // Does not display a drawer button in desktop mode
+  largeScreenHide: false, // Hides an element on smaller screens
+  smallScreenHide: false, // Hides an element on larger screens
+  // Computed
+  fixedDrawerClass: computed('fixedDrawer', function() {
+    if (this.get('fixedDrawer')) {
+      return 'mdl-layout--fixed-drawer';
+    }
+  }),
+  fixedHeaderClass: computed('fixedHeader', function() {
+    if (this.get('fixedHeader')) {
+      return 'mdl-layout--fixed-header';
+    }
+  }),
+  noDrawerButtonClass: computed('noDrawerButton', function() {
+    if (this.get('noDrawerButton')) {
+      return 'mdl-layout--no-drawer-button';
+    }
+  }),
+  noDesktopDrawerButtonClass: computed('noDesktopDrawerButton', function() {
+    if (this.get('noDesktopDrawerButton')) {
+      return 'mdl-layout--no-desktop-drawer-button';
+    }
+  }),
+});
---
+++ tests/integration/components/smd-app-test.js
@@ -0,0 +1,56 @@
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('smd-app', 'Integration | Component | smd app', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  this.set('fixedDrawer', false);
+  this.set('fixedHeader', false);
+  this.set('noDrawerButton', false);
+  this.set('noDesktopDrawerButton', false);
+
+  this.render(hbs(`{{smd-app fixedDrawer=fixedDrawer fixedHeader=fixedHeader noDrawerButton=noDrawerButton noDesktopDrawerButton=noDesktopDrawerButton}}`));
+
+  let $component = this.$('.ember-view').first();
+
+  assert.equal($component.hasClass('mdl-layout--fixed-drawer'), false, 'Does not have fixed drawer class');
+  assert.equal($component.hasClass('mdl-layout--fixed-header'), false, 'Does not have fixed header class');
+  assert.equal($component.hasClass('mdl-layout--no-drawer-button'), false, 'Does not have no drawer button class');
+  assert.equal($component.hasClass('mdl-layout--no-desktop-drawer-button'), false, 'Does not have no desktop drawer button class');
+
+  // turn on fixed drawer
+  this.set('fixedDrawer', true);
+  assert.equal($component.hasClass('mdl-layout--fixed-drawer'), true, 'Has fixed drawer class');
+
+  // turn on fixed header
+  this.set('fixedHeader', true);
+  assert.equal($component.hasClass('mdl-layout--fixed-header'), true, 'Has fixed header class');
+
+  // hide drawer button
+  this.set('noDrawerButton', true);
+  assert.equal($component.hasClass('mdl-layout--no-drawer-button'), true, 'Has no drawer button class');
+
+  // Hide drawer button on desktop
+  this.set('noDesktopDrawerButton', true);
+  assert.equal($component.hasClass('mdl-layout--no-desktop-drawer-button'), true, 'Has no desktop drawer button class');
+});
+
+test('it renders as with content', function(assert) {
+
+  // Template block usage:
+  this.render(hbs(`
+    {{#smd-app}}
+      template block text
+    {{/smd-app}}
+  `));
+
+  let $content = this.$('.page-content');
+
+  assert.equal($content.text().trim(), 'template block text', 'it renderes content');
+});
