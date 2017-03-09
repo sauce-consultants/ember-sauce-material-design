794317bda8161e431676df4efd1c174f42ff1fe9
Jim Wardlaw <jim.wardlaw@gmail.com>
2017-03-09 10:29:39 +0000

    Docs (layout): Updated header docs
    Added scrolling header and code snippets

---
+++ tests/dummy/app/snippets/layout.js
@@ -0,0 +1,6 @@
+// BEGIN-SNIPPET app-navigation
+// app/components/app-navigation.js
+import Navigation from 'ember-sauce-material-design/components/smd-navigation';
+
+export default Navigation.extend();
+// END-SNIPPET
