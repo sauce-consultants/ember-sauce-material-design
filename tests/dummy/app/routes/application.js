42357560093c75a8ec8bb79b73218b31c646b38d
Jim Wardlaw <jim.wardlaw@gmail.com>
2017-03-09 09:13:58 +0000

    Feat (docs): Added scroll to actions to applicaiton route

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -9,5 +9,21 @@ export default Route.extend({
     back: function() {
       this.transitionTo('home');
     },
+    jumpToTop: function() {
+      this.scrollTo('#top');
+    },
+    jumpToAttributes: function() {
+      this.scrollTo('#attributes');
+    },
+    jumpToActions: function() {
+      this.scrollTo('#actions');
+    },
   },
+  scrollTo: function(element) {
+    let position = $(element).offset().top - 128;
+    Ember.Logger.log(position);
+    $('.smd-page--scrollable').animate({
+      scrollTop: position
+    }, 200);
+  }
 });
