303be90bd0e1b6cf8858b28431c42b6399613a35
Jim Wardlaw <jim.wardlaw@gmail.com>
2017-03-13 20:51:19 +0000

    Feature (tabs): Added alignment attribute

--- addon/components/smd-tabs.js
+++ addon/components/smd-tabs.js
@@ -2,11 +2,18 @@ import Ember from 'ember';
 import layout from '../templates/components/smd-tabs';
 
 const {
-  Component
+  Component,
+  computed
 } = Ember;
 
 export default Component.extend({
+  // Attributes
   layout,
+  align: 'center', // left, right & center
   tagName: 'nav',
   classNames: ['mdl-tabs', 'mdl-js-tabs', 'mdl-js-ripple-effect'],
+  // Computed
+  tabBarClass: computed('align', function() {
+    return 'mdl-tabs__tab-bar--' + this.get('align');
+  })
 });
--- tests/integration/components/smd-tabs-test.js
+++ tests/integration/components/smd-tabs-test.js
@@ -1,4 +1,7 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('smd-tabs', 'Integration | Component | smd tabs', {
@@ -10,16 +13,35 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{smd-tabs}}`);
+  this.render(hbs `{{smd-tabs}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
-  this.render(hbs`
+  this.render(hbs `
     {{#smd-tabs}}
       template block text
     {{/smd-tabs}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+
+test('it renders with alignment class', function(assert) {
+
+  this.set('align', 'left');
+
+  this.render(hbs `{{smd-tabs align=align}}`);
+
+  let $tabContainer = this.$('.mdl-tabs__tab-bar');
+
+  assert.equal($tabContainer.hasClass('mdl-tabs__tab-bar--left'), true);
+
+  this.set('align', 'right');
+  assert.equal($tabContainer.hasClass('mdl-tabs__tab-bar--right'), true);
+
+  this.set('align', 'center');
+  assert.equal($tabContainer.hasClass('mdl-tabs__tab-bar--center'), true);
+
 });
