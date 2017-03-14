e7ee29d49ceb2abff74f257137433d43ccb65ab8
Matt Weldon <matt@mattweldon.com>
2017-03-14 09:53:51 +0000

    Fix (smd-button): Add `type` attribute to element correctly

--- addon/components/smd-button.js
+++ addon/components/smd-button.js
@@ -36,7 +36,7 @@ export default Component.extend(Clickable, {
   attributeBindings: [
     'disabled',
     'for',
-    'type:buttonType'
+    'type'
   ],
   // Computed
   _isMiniFabMode: computed('isMiniFab', function() {
