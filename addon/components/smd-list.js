dd0eb48e716a9d7b0daba95a82505693c6f545f8
Jim Wardlaw <jim.wardlaw@gmail.com>
2017-03-14 15:55:23 +0000

    Chore (lists): Remove dead code

--- addon/components/smd-list.js
+++ addon/components/smd-list.js
@@ -51,7 +51,6 @@ export default Component.extend({
       subHeight,
       top;
 
-
     if (currentScrollTop > lastScrollTop) {
       $nextSubheader = $currentSubheader.nextUntil('.smd-subheader').next('.smd-subheader');
       if ($nextSubheader.length) {
@@ -103,68 +102,6 @@ export default Component.extend({
     }
 
     this.set('lastScrollTop', currentScrollTop);
-  },
-  _onScroll: function($header, $body, scrollAmount, maxHeight) {
-
-    var currentScrollTop = Ember.$(this).scrollTop(),
-      $currentSubheader = $list.find('.smd-subheader--sticky'),
-      $nextSubheader,
-      $prevSubheader,
-      diff,
-      subHeight,
-      top;
-
-    if (currentScrollTop > lastScrollTop) {
-      $nextSubheader = $currentSubheader.nextUntil('.smd-subheader').next('.smd-subheader');
-      if ($nextSubheader.length) {
-        diff = $nextSubheader.offset().top - $list.offset().top;
-        subHeight = $currentSubheader.outerHeight();
-
-        if ($list.offset().top > $nextSubheader.offset().top) {
-          $currentSubheader.removeClass('smd-subheader--sticky');
-          $nextSubheader.addClass('smd-subheader--sticky');
-          $currentSubheader.css('top', '');
-        } else if (diff < subHeight) {
-          // This is where we make the next subheader push the current
-          // subheader off screen
-          top = diff - subHeight;
-          $currentSubheader.css('top', top);
-        }
-      }
-    } else {
-      $prevSubheader = $currentSubheader.prevUntil('.smd-subheader').prev('.smd-subheader');
-      $nextSubheader = $currentSubheader.nextUntil('.smd-subheader').next('.smd-subheader');
-
-      subHeight = parseInt($currentSubheader.outerHeight());
-
-      var $lastItem = $currentSubheader.next(),
-        lastItemOffset = $lastItem.offset().top - subHeight;
-
-      diff = $list.offset().top - lastItemOffset;
-
-      if ($prevSubheader.length) {
-        if ($list.offset().top < lastItemOffset) {
-          $currentSubheader.removeClass('smd-subheader--sticky');
-          $prevSubheader.addClass('smd-subheader--sticky');
-          $currentSubheader.css('top', '');
-        }
-      }
-      // This is where we make the previous subheader push the current
-      // subheader back on the list
-      if ($nextSubheader.length) {
-        if ($list.offset().top < $nextSubheader.offset().top) {
-          diff = $nextSubheader.offset().top - $list.offset().top;
-          if (diff >= 0 && diff < subHeight) {
-            top = (subHeight - diff) * -1;
-            $currentSubheader.css('top', top);
-          } else if (diff > subHeight) {
-            $currentSubheader.css('top', '');
-          }
-        }
-      }
-    }
-
-    lastScrollTop = currentScrollTop;
   },
   createSubHeaderClone: function($list) {
     // When the first header is given a fixed position,
