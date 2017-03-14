ff4a6379374378230ac323d5d8797d8dbcfd232e
Jim Wardlaw <jim.wardlaw@gmail.com>
2017-03-14 20:24:06 +0000

    Test (animation): Fixed utils test

--- tests/unit/utils/smd-transitions-test.js
+++ tests/unit/utils/smd-transitions-test.js
@@ -1,10 +1,15 @@
-import smdTransitions from 'dummy/utils/smd-transitions';
-import { module, test } from 'qunit';
+import {
+  tabTransitions
+} from 'ember-sauce-material-design/utils/smd-transitions';
+import {
+  module,
+  test
+} from 'qunit';
 
 module('Unit | Utility | smd transitions');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = smdTransitions();
+  let result = tabTransitions();
   assert.ok(result);
 });
