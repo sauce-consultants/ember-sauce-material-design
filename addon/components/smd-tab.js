be4350dfba8fd3d0093f4ae3140cb5856efe2e1d
Jim Wardlaw <jim.wardlaw@gmail.com>
2017-03-14 12:09:03 +0000

    Docs (tabs): Updated docs with smd-tab details

--- addon/components/smd-tab.js
+++ addon/components/smd-tab.js
@@ -1,9 +1,26 @@
 import Ember from 'ember';
 import layout from '../templates/components/smd-tab';
 
-export default Ember.LinkComponent.extend({
+const {
+  LinkComponent,
+  computed,
+  computed: {
+    alias
+  }
+} = Ember;
+
+export default LinkComponent.extend({
+  // Attributes
   layout,
   classNames: [
     'mdl-tabs__tab',
   ],
+  classNameBindings: [
+    'themeClass'
+  ],
+  // Computed
+  theme: alias('parentView.theme'),
+  themeClass: computed('theme', function() {
+    return 'mdl-tabs__tab--' + this.get('theme');
+  }),
 });
