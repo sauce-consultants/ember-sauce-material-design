41bb79f3eb384ab7388f04af8362c968dcb0722b
Jim Wardlaw <jim.wardlaw@gmail.com>
2017-03-27 14:22:28 +0100

    Fix (fab): Call super on fabable route route actions

--- addon/mixins/smd-fabable-route.js
+++ addon/mixins/smd-fabable-route.js
@@ -2,35 +2,39 @@ import Ember from 'ember';
 
 const {
   Mixin,
-  inject:{service},
+  inject: {
+    service
+  },
 } = Ember;
 
 export default Mixin.create({
   // Services
-  fab:service("smd-fab-service"),
+  fab: service("smd-fab-service"),
   // Attributes
-  fabIcon:"add",
+  fabIcon: "add",
   fabAlign: "right",
-  fabIsSmall:false,
-  fabIsAccent:false,
-  fabIsPrimary:true,
-  fabDisabled:false,
-  fabIsHidden:false,
-  fabAction:"fabAction",
+  fabIsSmall: false,
+  fabIsAccent: false,
+  fabIsPrimary: true,
+  fabDisabled: false,
+  fabIsHidden: false,
+  fabAction: "fabAction",
   // Methods
-  activate: function () {
+  activate: function() {
+    this._super();
     this.get("fab").setup({
-      icon:this.get("fabIcon"),
+      icon: this.get("fabIcon"),
       align: this.get("fabAlign"),
-      isSmall:this.get("fabIsSmall"),
-      isAccent:this.get("fabIsAccent"),
-      isPrimary:this.get("fabIsPrimary"),
-      disabled:this.get("fabDisabled"),
-      isHidden:this.get("fabIsHidden"),
-      action:this.get("fabAction"),
+      isSmall: this.get("fabIsSmall"),
+      isAccent: this.get("fabIsAccent"),
+      isPrimary: this.get("fabIsPrimary"),
+      disabled: this.get("fabDisabled"),
+      isHidden: this.get("fabIsHidden"),
+      action: this.get("fabAction"),
     });
   },
-  deactivate: function () {
+  deactivate: function() {
+    this._super();
     this.get("fab").clear();
   },
 });
