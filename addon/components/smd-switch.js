1fb6393bb32ac37099d27fd089e04fae35032ddf
Alex Pringle <alex@alexpringle.co.uk>
2017-04-04 12:26:20 +0100

    add smd-switch component that fixes mdl-switch disabled bug

---
+++ addon/components/smd-switch.js
@@ -0,0 +1,6 @@
+import layout from '../templates/components/smd-switch';
+import MdlSwitch from 'ember-material-lite/components/mdl-switch';
+
+export default MdlSwitch.extend({
+  layout
+});
---
+++ tests/integration/components/smd-switch-test.js
@@ -0,0 +1,6 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('smd-switch', 'Integration | Component | smd switch', {
+  integration: true
+});
