bea3090fdcb7983d8e71921f45a9db2678cbe4d6
Jim Wardlaw <jim.wardlaw@gmail.com>
2017-05-31 16:38:38 +0100

    Feat (data-item): Add action and class to data items

--- addon/components/smd-data-item.js
+++ addon/components/smd-data-item.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import layout from '../templates/components/smd-data-item';
+import Clickable from '../mixins/click-support';
 
 const {
   Component,
@@ -9,10 +10,18 @@ const {
   },
 } = Ember;
 
-export default Component.extend({
+export default Component.extend(Clickable, {
   // Attributes
   layout,
-  classNames: ['smd-data-list__item'],
+  classNames: [
+    'smd-data-list__item',
+    'smd-data-list__value',
+  ],
+  classNameBindings: [
+    'hasAction:smd-data-list__value--with-action',
+    'isMultiLine:smd-data-list__value--multi-line',
+    'isSmall:smd-data-list__value--small'
+  ],
   label: null,
   url: null,
   value: null,
@@ -20,21 +29,12 @@ export default Component.extend({
   isMultiLine: false,
   isSmall: false,
   // Computed
+  hasAction: bool('action'),
   hasLabel: bool('label'),
   hasValue: bool('value'),
   hasUrl: bool('url'),
   hasIcon: bool('icon'),
   niceUrl: computed('url', function() {
     return this.get('url').replace(/(^\w+:|^)\/\//, '');
-  }),
-  valueClasses: computed('isMultiLine', 'isSmall', function() {
-    var classNames = ['smd-data-list__value'];
-    if (this.get('isMultiLine')) {
-      classNames.push('smd-data-list__value--multi-line');
-    }
-    if (this.get('isSmall')) {
-      classNames.push('smd-data-list__value--small');
-    }
-    return classNames.join(' ');
   }),
 });
