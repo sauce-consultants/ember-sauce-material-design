12f305bdef1e76235ec6ad798e8fdcfe188efb2c
Alex Pringle <alex@alexpringle.co.uk>
2017-07-25 15:59:33 +0100

    add options and extraClass to smd-toaster (snackbar)

--- addon/services/smd-toaster.js
+++ addon/services/smd-toaster.js
@@ -2,14 +2,30 @@ import Ember from "ember";
 
 export default Ember.Service.extend({
   message: null,
-  setMessage(text) {
+  _extraClass: null,
+  setMessage(text,options={},newExtraClass=null) {
+
     this.set('message', text);
+
+    let snackbarElement = Ember.$('.mdl-snackbar');
+
+    let currentExtraClass = this.get('_extraClass');
+    if(currentExtraClass)
+    {
+      snackbarElement.removeClass(currentExtraClass);
+    }
+    if(newExtraClass)
+    {
+      snackbarElement.addClass(newExtraClass);
+    }
 
     var data = {
       message: this.get('message'),
       timeout: 2000
     };
-    Ember.$('.mdl-snackbar').get(0).MaterialSnackbar.showSnackbar(data);
+    Ember.merge(data,options);
+
+    snackbarElement.get(0).MaterialSnackbar.showSnackbar(data);
   },
   getMessage() {
     return this.get('message');
